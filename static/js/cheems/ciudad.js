1da3a2c01887150f71c95d8115c71b42eb96c758
jcecena <jantonio.cecena@gmail.com>
2024-11-19 19:51:26 -0700

    Se agregó modal de ciudad

--- static/js/cheems/ciudad.js
+++ static/js/cheems/ciudad.js
@@ -2,7 +2,7 @@ function registrar(id){
     var url = "/ciudad-registro"
     $.get(url, function(data){
         $('#modal_info').html(data);
-        //$('modal_info').modal({ backdrop: 'static', keyboard: false});
+        $('#modal_info').modal({ backdrop: 'static', keyboard: false});
         $('#modal_info').modal('show');
     })
 }
